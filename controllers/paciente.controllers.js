23b6fcb2d11c34ecd2d7ed2e75f7b54e6de61cd1
LaureanoVera <laureanoigvera@gmail.com>
2021-08-11 09:10:30 -0300

    creando controlador

---
+++ controllers/paciente.controllers.js
@@ -0,0 +1,6 @@
+
+// CUANDO SE CREA UN NUEVO CLIENTE
+exports.nuevoCliente = (req, res, next) => {
+  // TODO: Insertar en la base de datos
+  res.json({mensaje: 'El cliente se agrego correctamente'})
+} 
