6dd150622ddca4abcb21b965d33add3d5feef541
LaureanoVera <laureanoigvera@gmail.com>
2021-08-11 09:03:52 -0300

    conectando a mongodb

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require('mongoose')
 const app = express();
-const port = process.env.PORT || 3000;
+
+// CONECT TO MONGODB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
 
 // SERVER LISTENING
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 })
